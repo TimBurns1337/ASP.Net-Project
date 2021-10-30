af9e9e21efe4b4a0a4474e21ecf38ea164864b5c
Tim Burns <tim.marshall.burns95@gmail.com>
2021-10-30 12:23:59 -0400

    set up first pc model

---
+++ Controllers/GamingPCController.cs
@@ -0,0 +1,35 @@
+﻿using ASP.Net_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_project.Controllers
+{
+    public class GamingPCController : Controller
+    {
+        IList<GamingPC> gamingPCs = new List<GamingPC>
+        { new GamingPC()
+        { Pc_Id = 1, Componets = "High Performance Gaming PC " +
+                                    "CPU: AMD Ryzen 9 5950X $800" +
+                                    "GPU: Nvidia GeForce RTX 3090 $1500" +
+                                    "Motherboard: Asus ROG Maximus XII Extreme $1000" +
+                                    "RAM: TEAM XTREEM ARGB 16GB DDR4-3600MHz C14 $500" +
+                                    "Hard Drive: Addlink S70 SSD 2TB $150" +
+                                    "Power: Corsair AX1600i 1600W $1000" +
+                                    "Cooling: Corsair H115i RGB Platinum $200"+
+                                    "Case: PC-O11D Razer Mid Tower $250" +
+            "Final Price $4000",
+            Summary = "CPU: AMD Ryzen 9 5950X $800 GPU: Nvidia GeForce RTX 3090 $1500, Final Price: $4000",
+            Image_Link = "../Images/HP.jfif", 
+            Price = 4000
+        } };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/GamingPC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_project.Models
+{
+    public class GamingPC
+    {
+        public int Pc_Id { get; set; }
+        // see all the details of the pc 
+        public string Componets { get; set; }
+        // proived a shorted view of components - cpu and gpu 
+        public string Summary { get; set; }
+        public string Image_Link { get; set; }
+        public int Price { get; set; }
+
+    }
+}
