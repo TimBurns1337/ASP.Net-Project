f0fae39998e2597f2aac5029982e02047d3cc462
Cezmi Aktepe <cezmi.aktepe04@gmail.com>
2021-11-01 21:30:15 -0400

    tried to fix validation but failed

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -10,28 +10,39 @@ namespace ASP.Net_project.Models
     public class Purchase
     {
         public int PurchaseId { get; set; }   
+        
         [Required]
         public int ComputerId { get; set; }
+        
         [Required]
         public string Fname { get; set; }
+        
         [Required]
         public string Lname { get; set; }
+        
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+        
         [Phone]
         public string Phone { get; set; }
+        
         [Required]
         public string Address { get; set; }
+        
         [Required]
         [RegularExpression("/^[A-Za-z]\\d[A-Za-z][ -]?\\d[A-Za-z]\\d$/")]
         public string PCode { get; set; }
+        
         [CreditCard]
         public string CardNo { get; set; }
+       
         [Required]
         public string CardExpDate { get; set; }
+       
         [Required]
         public int CvcNo { get; set; }
+       
         [Required]
         public string CardHolderName { get; set; }
     }
