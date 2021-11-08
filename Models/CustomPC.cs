cdc250a074c3ca06e503cb3f6f2bd0a70b73ac62
Tim Burns <tim.marshall.burns95@gmail.com>
2021-11-08 12:14:23 -0500

    Created Custom PC model and index view, no data added so far

---
+++ Models/CustomPC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_project.Models
+{
+    public class CustomPC
+    {
+        public int Cust_ID { get; set; }
+        public string CPU { get; set; }
+        public string GPU { get; set; }
+        public string MotherBoard { get; set; }
+        public string RAM { get; set; }
+        public string HDD { get; set; }
+        public string Power { get; set; }
+        public string Cooling { get; set; }
+        public string Case { get; set; }
+    }
+}
